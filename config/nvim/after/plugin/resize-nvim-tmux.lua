55c20281e8e8544d16f092c542e8e76597d012ca
Roberto Albanesi <beto0607@gmail.com>
2024-03-03 14:02:52 +0100

    Add resize-nvim-tmux

---
+++ config/nvim/after/plugin/resize-nvim-tmux.lua
@@ -0,0 +1,11 @@
+local wr_group = vim.api.nvim_create_augroup('WinResize', { clear = true })
+
+vim.api.nvim_create_autocmd(
+    'VimResized',
+    {
+        group = wr_group,
+        pattern = '*',
+        command = 'wincmd =',
+        desc = 'Automatically resize windows when the host window size changes.'
+    }
+)
