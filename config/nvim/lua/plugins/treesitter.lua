05d659018ff5f5980cf685316084e4112fb5b203
Roberto Albanesi <beto0607@gmail.com>
2024-06-23 03:05:59 +0200

    More installs

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,27 +2,8 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "angular",
-        "tsx",
-        "typescript",
-      })
+    build = ":TSUpdate",
+    opts = function(_)
       vim.filetype.add({
         pattern = {
           [".*%.component%.html"] = "angular.html", -- Sets the filetype to `angular.html` if it matches the pattern
@@ -32,10 +13,55 @@ return {
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "angular.html",
         callback = function()
-          print("potatos")
           vim.treesitter.language.register("angular", "angular.html") -- Register the filetype with treesitter for the `angular` language/parser
         end,
       })
+    end,
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "bash",
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "query",
+          "regex",
+          "tsx",
+          "typescript",
+          "vim",
+          "yaml",
+          "angular",
+        },
+
+        sync_install = false,
+
+        auto_install = true,
+
+        indent = {
+          enable = true,
+        },
+
+        highlight = {
+          enable = true,
+
+          additional_vim_regex_highlighting = { "markdown" },
+        },
+      })
+
+      local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      treesitter_parser_config.templ = {
+        install_info = {
+          url = "https://github.com/vrischmann/tree-sitter-templ.git",
+          files = { "src/parser.c", "src/scanner.c" },
+          branch = "master",
+        },
+      }
+
+      vim.treesitter.language.register("templ", "templ")
     end,
   },
 }
