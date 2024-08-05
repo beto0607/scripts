b69c83d39f8c5fbd1467b8a6e524eace1f660d01
Roberto Albanesi <beto0607@gmail.com>
2024-08-05 11:33:46 +0200

    Add fzf to new_worktree. Fix noice not showing recording

---
+++ config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    routes = {
+      {
+        view = "notify",
+        filter = { event = "msg_showmode" },
+      },
+    },
+  },
+}
