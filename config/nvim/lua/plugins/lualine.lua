263e0fb6af944b932b20dfe4528c754cefb32a0f
Roberto Albanesi <beto0607@gmail.com>
2024-08-05 12:45:08 +0200

    move recording text to lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -7,6 +7,15 @@ return {
       return {
         icons_enabled = true,
         theme = "catppuccin",
+        sections = {
+          lualine_c = {
+            {
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+              color = { fg = "#ff9e64" },
+            },
+          },
+        },
       }
     end,
   },
--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -1,12 +1,5 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {
-    routes = {
-      {
-        view = "notify",
-        filter = { event = "msg_showmode" },
-      },
-    },
-  },
+  opts = {},
 }
