1641f723cf8e0da570dd2add9a9d69083b1e761e
Roberto Albanesi <beto0607@gmail.com>
2024-09-30 13:54:33 +0200

    Add mason-dap

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -1,51 +1,44 @@
 return {
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "haydenmeade/neotest-jest",
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "haydenmeade/neotest-jest",
+        },
+        keys = {
+            {
+                "<leader>tl",
+                function()
+                    require("neotest").run.run_last()
+                end,
+                desc = "Run Last Test",
+            },
+            {
+                "<leader>tL",
+                function()
+                    require("neotest").run.run_last({ strategy = "dap" })
+                end,
+                desc = "Debug Last Test",
+            },
+            {
+                "<leader>tT",
+                function()
+                    require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" })
+                end,
+                desc = "Debug Last Test",
+            },
+        },
+        opts = function(_, opts)
+            table.insert(
+                opts.adapters,
+                require("neotest-jest")({
+                    jestCommand = "npx jest run",
+                    jestConfigFile = "custom.jest.config.ts",
+                    env = { CI = true },
+                    cwd = function()
+                        return vim.fn.getcwd()
+                    end,
+                })
+            )
+        end,
     },
-    keys = {
-      {
-        "<leader>tl",
-        function()
-          require("neotest").run.run_last()
-        end,
-        desc = "Run Last Test",
-      },
-      {
-        "<leader>tL",
-        function()
-          require("neotest").run.run_last({ strategy = "dap" })
-        end,
-        desc = "Debug Last Test",
-      },
-      {
-        "<leader>tT",
-        function()
-          -- require("neotest").run.file({ strategy = "dap" })
-            require("neotest").run.run({vim.fn.expand("%"), strategy = "dap"})
-
-        end,
-        desc = "Debug Last Test",
-      },
-      {
-        "<leader>tw",
-        "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
-        desc = "Run Watch",
-      },
-    },
-    opts = function(_, opts)
-      table.insert(
-        opts.adapters,
-        require("neotest-jest")({
-          jestCommand = "npx jest run",
-          jestConfigFile = "custom.jest.config.ts",
-          env = { CI = true },
-          cwd = function()
-            return vim.fn.getcwd()
-          end,
-        })
-      )
-    end,
-  },
 }
