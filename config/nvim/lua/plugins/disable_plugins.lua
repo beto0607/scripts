6b2584dee0fd74b864792ffa8eb710fe623e3218
Roberto Albanesi <beto0607@gmail.com>
2025-01-16 18:31:16 +0100

    Update lazy-lock. Remove jira scripts

--- config/nvim/lua/plugins/disable_plugins.lua
+++ config/nvim/lua/plugins/disable_plugins.lua
@@ -4,4 +4,6 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "lewis6991/gitsigns.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 }
--- config/nvim/lua/plugins/nvim-dap.lua
+++ config/nvim/lua/plugins/nvim-dap.lua
@@ -36,7 +36,6 @@ return {
       command = "/home/beto0607/App/netcoredbg/netcoredbg",
       args = { "--interpreter=vscode" },
     }
-
     dap.configurations.cs = {
       {
         type = "coreclr",
@@ -66,6 +65,49 @@ return {
         -- attach = function()
         --   return vim.fn.input("Process ID", "")
         -- end,
+      },
+    }
+
+    dap.adapters.delve = function(callback, config)
+      if config.mode == "remote" and config.request == "attach" then
+        callback({
+          type = "server",
+          host = config.host or "127.0.0.1",
+          port = config.port or "38697",
+        })
+      else
+        callback({
+          type = "server",
+          port = "${port}",
+          executable = {
+            command = "dlv",
+            args = { "dap", "-l", "127.0.0.1:${port}", "--log", "--log-output=dap" },
+            detached = vim.fn.has("win32") == 0,
+          },
+        })
+      end
+    end
+    dap.configurations.go = {
+      {
+        type = "delve",
+        name = "Debug",
+        request = "launch",
+        program = "main.go",
+      },
+      {
+        type = "delve",
+        name = "Debug test", -- configuration for debugging test files
+        request = "launch",
+        mode = "test",
+        program = "${file}",
+      },
+      -- works with go.mod packages and sub packages
+      {
+        type = "delve",
+        name = "Debug test (go.mod)",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}",
       },
     }
   end,
